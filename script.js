3c8f5e8af8ed69cc555a0638b33d3b0cba89e0f9
Rasmus H <99361520+rasmushaislund@users.noreply.github.com>
2023-02-21 10:45:04 +0100

    Add DOM and non-DOM related variables

--- script.js
+++ script.js
@@ -1,9 +1,50 @@
 // Start
 
-let valueOne;
-let valueTwo;
-let operator;
-let result = valueOne
+// DOM VARIABLES
+
+const sliderThousandSeparator = document.querySelector("#thousand-sep");
+const sliderDecimalSeparator = document.querySelector("#decimal-sep");
+
+const deleteIcon = document.querySelector("#delete-icon");
+
+const clearAll = document.querySelector("#clear-all");
+const plusMinus = document.querySelector("#plus-minus");
+const percent = document.querySelector("#percent");
+
+const division = document.querySelector("#division");
+const multiplication = document.querySelector("#multiplication");
+const subtraction = document.querySelector("#subtraction");
+const addition = document.querySelector("#addition");
+const equal = document.querySelector("#equal");
+
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+const decimal = document.querySelector("#decimal");
+
+
+// NON-DOM VARIABLES
+
+let valueOne = inputNumbers[inputNumbers.length-2]
+let valueTwo = inputNumbers[inputNumbers.length-1]
+let latestOperator;
+let inputNumbers = [];
+let displayCalc = []
+let result;
+let decimalEntered = false;
+let commaAsThousandSeparator = false;
+let dotAsDecimalSeparator = false;
+
+
+// SLIDER FEATURES
+
 
 
 // MATH OPERATIONS
